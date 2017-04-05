5402acc0009905eb67775c4b53a1bc110b942179
samueldoud <sd8396a@student.american.edu>
2017-04-05 15:42:07 -0400

    Adding tkinter framework

--- tomography/src/Display.py
+++ tomography/src/Display.py
@@ -1,19 +1,31 @@
 """A user interface for interacting with the network."""
 
+from tkinter import Tk, Frame
+
 import Tomography
 
-class Display(object):
+class Display(Frame):
     """Provide the user with a visualization of the network and a way to interact with it."""
-    def __init__(self):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.master = master
+        self.master.title("Tomography")
+        self.master.protocol("WM_DELETE_WINDOW", self.shutdown)
         self.tomography = Tomography.Tomography()
 
     def tick(self, time=1):
         """Increment n units of time."""
         if time < 1:
             raise Exception("Must provide a positive real value for time" +
-                            "(Although it really should be an integer you oafs.)")
+                            "(Although it really should be an integer you oaf.)")
         for _counter in range(time):
             self.tomography.tick()
+        self.draw()
+
+    def shutdown(self):
+        """Closes the application 'gracefully'."""
+        self.master.quit()
+        self.master.destroy()
 
     def add_node(self, node):
         """Add a node to the Tomography.
@@ -27,3 +39,11 @@ class Display(object):
     def connect_nodes(self, start_node, end_node):
         """Connect two Nodes. A start node should be upstream of the end_node.ß"""
         self.tomography.add_connection(start_node, end_node)
+
+    def draw(self):
+        """Draw all nodes and their connections (along with any notation about data flows)."""
+        pass
+
+ROOT = Tk()
+WINDOW = Display(master=ROOT)
+WINDOW.mainloop()
